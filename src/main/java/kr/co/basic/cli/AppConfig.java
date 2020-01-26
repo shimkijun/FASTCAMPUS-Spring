e324b0a8bdee20cea826d6a60adb23ef23997416
shimkijun <shimkijun@gmail.com>
2020-01-26 13:41:51 +0900

    Spring IOC Environment Abstraction 1 and 2

--- src/main/java/kr/co/basic/cli/AppConfig.java
+++ src/main/java/kr/co/basic/cli/AppConfig.java
@@ -1,11 +1,16 @@
 package kr.co.basic.cli;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
+
+import java.sql.Connection;
 
 @Configuration
-@ComponentScan(basePackageClasses = AppConfig.class)
+@Profile({"default","dev"})
+@PropertySource("classpath:application-${spring.profiles.active}.properties")
 public class AppConfig {
     @Bean
     public B b(){
@@ -15,5 +20,25 @@ public class AppConfig {
     @Bean(initMethod = "init" , destroyMethod = "destroy")
     public A a(B b){
         return new A(b);
+    }
+
+    @Bean(initMethod = "init",destroyMethod = "destroy")
+    public ConnectionFactory connectionFactory(
+            @Value("${jdbc.driverClass}") String driverClass,
+            @Value("${jdbc.url}") String url,
+            @Value("${jdbc.user}") String user,
+            @Value("${jdbc.password}") String password
+    ){
+        return new ConnectionFactory(driverClass,url,user,password);
+    }
+
+    @Bean
+    public Connection connection(ConnectionFactory connectionFactory){
+        return connectionFactory.getConnection();
+    }
+
+    @Bean
+    public Dao dao(Connection connection){
+        return new Dao(connection);
     }
 }
--- src/main/java/kr/co/basic/cli/Main.java
+++ src/main/java/kr/co/basic/cli/Main.java
@@ -2,18 +2,15 @@ package kr.co.basic.cli;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @Slf4j
 public class Main {
     public static void main(String[] args){
-        ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("dao.xml");
-//        context.scan("kr.co.basic.cli");
-//        context.register(AppConfig.class);
-//        context.refresh();
-//        B b = context.getBean(B.class);
-//        log.info("{}",b);
-//        context.close();
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.register(AppConfig.class);
+        context.refresh();
         Dao dao = context.getBean(Dao.class);
         dao.run();
         context.close();
