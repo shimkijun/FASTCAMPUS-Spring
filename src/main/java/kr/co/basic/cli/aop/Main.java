7fc54fbcdd10d4daab8e853d7f7eb16a25dafcb9
shimkijun <shimkijun@gmail.com>
2020-01-27 12:36:55 +0900

    AOP xml before,after,returning,throwing

--- src/main/java/kr/co/basic/cli/aop/Main.java
+++ src/main/java/kr/co/basic/cli/aop/Main.java
@@ -1,44 +1,11 @@
 package kr.co.basic.cli.aop;
-
-import org.aopalliance.aop.Advice;
-import org.aopalliance.intercept.MethodInterceptor;
-import org.aopalliance.intercept.MethodInvocation;
-import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        ProxyFactory factory = new ProxyFactory(new SimplePojo());
-        factory.addInterface(Pojo.class);
-        factory.addAdvice(new RetryAdvice());
-
-        Pojo pojo = (Pojo) factory.getProxy();
-        System.out.println("start");
-        pojo.foo();
-        System.out.println("end");
-    }
-}
-
-class RetryAdvice implements MethodInterceptor {
-    @Override
-    public Object invoke(MethodInvocation methodInvocation) throws Throwable {
-        System.out.println("before");
-        Object proceed = methodInvocation.proceed();
-        System.out.println("after");
-        return proceed;
-    }
-}
-
-interface Pojo{
-    void foo();
-}
-
-class SimplePojo implements Pojo{
-    public void bar() {
-        System.out.println("run foo");
-    }
-
-    @Override
-    public void foo() {
-        this.bar();
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
+        Service serviceBean = context.getBean(Service.class);
+        serviceBean.log();
+        context.close();
     }
 }
