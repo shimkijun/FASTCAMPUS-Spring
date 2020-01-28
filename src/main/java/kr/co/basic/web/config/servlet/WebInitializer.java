31d91c9498cd6db4e009e1baa7fab2398a9bb41e
shimkijun <shimkijun@gmail.com>
2020-01-28 15:56:12 +0900

    Spring-framework study finish

--- src/main/java/kr/co/basic/web/config/servlet/WebInitializer.java
+++ src/main/java/kr/co/basic/web/config/servlet/WebInitializer.java
@@ -3,30 +3,22 @@ package kr.co.basic.web.config.servlet;
 import kr.co.basic.web.config.spring.AppConfig;
 import kr.co.basic.web.config.spring.MvcConfig;
 import kr.co.basic.web.config.spring.SecurityConfig;
-import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.ContextLoaderListener;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRegistration;
-
-public class WebInitializer implements WebApplicationInitializer {
+public class WebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
     @Override
-    public void onStartup(ServletContext container) throws ServletException {
-        AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
-        rootContext.register(AppConfig.class);
-        rootContext.register(MvcConfig.class);
-        rootContext.register(SecurityConfig.class);
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[] {AppConfig.class, MvcConfig.class, SecurityConfig.class};
+    }
 
-        container.addListener(new ContextLoaderListener(rootContext));
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return null;
+    }
 
-        AnnotationConfigWebApplicationContext dispatcherContext = new AnnotationConfigWebApplicationContext();
-
-        ServletRegistration.Dynamic dispatcher = container.addServlet("dispatcher", new DispatcherServlet(dispatcherContext));
-        dispatcher.setLoadOnStartup(1);
-        dispatcher.addMapping("/");
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
     }
 }
